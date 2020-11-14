fc1b44096f3a7621aeddb71f1ce0378d58b8c19e
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2020-11-14 23:09:42 +0100

    feat: render profile information

--- public/index.js
+++ public/index.js
@@ -49,6 +49,34 @@ const graphqlQuery = `{
     }
 }`
 
+const profileData = (data) => {
+    const pictures = document.querySelectorAll(".picture");
+    pictures.forEach((picture) => {
+        picture.src = data.avatarUrl;
+    });
+    const names = document.querySelectorAll(".name");
+    names.forEach((name) => {
+        name.innerHTML = data.name;
+    });
+    const usernames = document.querySelectorAll(".username");
+    usernames.forEach((username) => {
+        username.innerHTML = data.login;
+    });
+    const bios = document.querySelectorAll(".bio");
+    bios.forEach((bio) => {
+        bio.innerHTML = data.bio;
+    });
+    const focus = document.querySelector(".focus")
+    focus.innerHTML = data.status.emojiHTML
+    // const website = document.querySelector("#website");
+    // website.href = "//" + user.websiteUrl;
+}
+
+const setData = (data) => {
+    profileData(data.user);
+    console.log(data.user);
+}
+
 const getDataFromGithub = () => {
     fetch("https://api.github.com/graphql", {
         method: "POST",
@@ -60,7 +88,10 @@ const getDataFromGithub = () => {
         body: JSON.stringify({query: graphqlQuery}),
     })
         .then(res => res.json())
-        .then(res => console.log(res.data))
+        .then(res => {
+            setData(res.data)
+            console.log(res)
+        })
         .catch(err => {
             console.log(err)
         })
